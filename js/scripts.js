544ab092ccbb5ad8f697756f9e1f9bae2c34f848
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-02-18 17:52:36 -0800

    adds a function to add a new item

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
 
 function newItem(){
 
-    //javascript
+    //jquery
     //1. Adding a new item to the list of items: 
-       let li = document.createElement("li");
-       let inputValue = document.getElementById("input").value;
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
+       let li = $("<li></li>")
+       let inputValue = $("#input").val();
+       li.appendChild(inputValue);
+
     
        if (inputValue === '') {
          alert("You must write something!");
        } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
+         $('#list').append(li);
        }
     
      //2. Crossing out an item from the list of items:
        function crossOut() {
-             li.classList.toggle("strike");
+            li.on("dblclick", function(){
+              li.addClass(".strike").fadeToggle();  
+            })
+             
          }
-    
-         li.addEventListener("dblclick",crossOut);
+        
+        crossOut();
     
      //3(i). Adding the delete button "X": 
        let crossOutButton = document.createElement("crossOutButton");
